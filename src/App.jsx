74a666775c0cc846f46a46c219ce50e23e0c00b6
Jayaditya <gaikwadjayaditya@gmail.com>
2023-01-03 16:27:57 +0530

    Added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import React from "react";
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Switch>
+      <Route exact path="/">
+        This is the home page
+      </Route>
 
-    </div>
+      <Route exact path="/starred">
+        This is starred page
+      </Route>
+      <Route>
+        404 Not Found
+      </Route>
+    
+
+    </Switch>
   );
 }
 
