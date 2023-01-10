0e38f7d41e0ccd8900aa3111f4f8e254a77ae3e4
Jayaditya <gaikwadjayaditya@gmail.com>
2023-01-10 16:53:34 +0530

    styled show page

---
+++ src/components/show/Seasons.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { SeasonList, SeasonsWrapper } from './Seasons.styled';
+
+const Seasons = ({ seasons }) => {
+    return (
+      <SeasonsWrapper>
+        <p>
+          Seasons in total: <span>{seasons.length}</span>
+        </p>
+        <p>
+          Episodes in total:{' '}
+          <span>
+            {seasons.reduce((acc, season) => acc + season.episodeOrder, 0)}
+          </span>
+        </p>
+
+        <SeasonList>
+          {seasons.map(season => (
+            <div className='season-item' key={season.id}>
+              <div className='left'>
+                <p>Season {season.number}</p>
+                <p>
+                  Episodes: <span>{season.episodeOrder}</span>
+                </p>
+              </div>
+              
+              <div className='right'>
+                Aired:{' '}
+                <span>
+                  {season.premiereDate} - {season.endDate}
+                </span>
+              </div>
+            </div>
+          ))}
+        </SeasonList>
+      </SeasonsWrapper>
+    );
+  };
+  
+
+export default Seasons
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,19 +1,30 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
+import { ShowPageWrapper, InfoBlock } from './Show.styled';
+
+
 
 const reducer = (prevState, action) => {
   switch (action.type) {
     case 'FETCH_SUCCESS': {
-      return {isLoading: false, error: null, show: action.show };
+      return { isLoading: false, error: null, show: action.show };
     }
     case 'FETCH_FAILED': {
-      return {...prevState, isLoading: false, error: action.error };
+      return { ...prevState, isLoading: false, error: action.error };
     }
     default:
       return prevState;
   }
 };
+
+
 const initialState = {
   show: null,
   isLoading: true,
@@ -21,13 +32,11 @@ const initialState = {
 };
 
 const Show = () => {
+
   const { id } = useParams();
 
-  const [state, dispatch] = useReducer(
-    reducer, 
-    initialState
-    );
-    console.log('state',state);
+  const [{show, isLoading,error}, dispatch] = useReducer(reducer, initialState);
+
 
   useEffect(() => {
     let isMounted = true;
@@ -35,13 +44,12 @@ const Show = () => {
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(results => {
         if (isMounted) {
-          dispatch({type: 'FETCH_SUCCESS',show: results});
+          dispatch({ type: 'FETCH_SUCCESS', show: results });
         }
       })
       .catch(err => {
         if (isMounted) {
-          dispatch({type: 'FETCH_FAILED',error: err.message});
-
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
         }
       });
 
@@ -54,13 +62,42 @@ const Show = () => {
   // console.log('isLoading', isLoading);
   // console.log('error', error);
 
-  // if (isLoading) {
-  //   return <div> loading....</div>;
-  // }
-  // if (error) {
-  //   return <div>Error Occurred:{error}</div>;
-  // }
-  return <div>this is show page</div>;
+  if (isLoading) {
+    return <div> loading....</div>;
+  }
+  if (error) {
+    return <div>Error Occurred:{error}</div>;
+  }
+  return (
+    <ShowPageWrapper>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+
+      <InfoBlock>
+        <h2> Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </InfoBlock>
+
+      <InfoBlock>
+        <h2> Seasons</h2>
+        <Seasons seasons={show._embedded.seasons}/>
+      </InfoBlock>
+
+      <InfoBlock>
+        <h2> Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </InfoBlock>
+    </ShowPageWrapper>
+  );
 };
 
 export default Show;
