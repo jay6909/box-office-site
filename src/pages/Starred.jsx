e919a46d89e07aa37ce0758a03e02654f28a65bd
Jayaditya <gaikwadjayaditya@gmail.com>
2023-01-10 18:24:47 +0530

    finished favourite page

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import MainPageLayout from '../components/MainPageLayout'
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+import { useShows } from '../misc/custom-hooks';
+import { apiGet } from '../misc/config';
+import ShowGrid from '../components/show/ShowGrid';
 
 const Starred = () => {
-  return (
-    <MainPageLayout>This is Starred</MainPageLayout>
-  )
-}
+  const [fav] = useShows();
 
-export default Starred
+  const [shows, setShows] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (fav && fav.length > 0) {
+      const promises = fav.map(showId => apiGet(`/shows/${showId}`));
+
+      Promise.all(promises)
+      .then(apiData=>apiData.map(show=>({show})))
+        .then(results => {
+          setShows(results);
+          setIsLoading(false);
+        })
+        .catch(err => {
+          setError(err.message);
+          setIsLoading(false);
+        });
+    } else {
+      setIsLoading(false);
+    }
+  }, [fav]);
+
+  return (
+    <MainPageLayout>
+      {isLoading && <div>wait a moment...loading...</div>}
+      {error && <div>Error happened while loading: {error}</div>}
+      {!isLoading &&!shows &&<div> You currently do not have favorites added, add some to see here</div>}
+      {!isLoading && !error && shows && <ShowGrid data={shows}/>}
+
+    </MainPageLayout>
+  );
+};
+
+export default Starred;
