baedef3f43f6b6146ccabe6e8e154cb3c69d25af
Jayaditya <gaikwadjayaditya@gmail.com>
2023-01-15 22:34:46 +0530

    defined theme for styled components and created a few styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import {Switch, Route} from 'react-router-dom';
+import {ThemeProvider} from 'styled-components'
+
 import Home from "./pages/Home";
 import Show from "./pages/Show";
 import Starred from "./pages/Starred";
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
     <Switch>
       <Route exact path="/">
         <Home/>
@@ -28,7 +37,7 @@ function App() {
     
 
     </Switch>
-    </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/Navs.jsx
+++ src/components/Navs.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { useLocation } from 'react-router'
+import { NavList, LinkStyled } from './Navs.styled'
 
 const LINKS=[
     {to:'/', text:'Home'},
     {to:'/starred', text:'Favorites'}
   ]
 const Navs = () => {
+  const location=useLocation();
   return (
     <div>
 
 
-    <ul>
+    <NavList>
       {
-        LINKS.map(item=><li key={item.to}><Link to={item.to}>{item.text}</Link></li>)
+        LINKS.map(item=><li key={item.to}>
+          <LinkStyled to={item.to}className={item.to===location.pathname ? 'active' : ''}>{item.text}</LinkStyled></li>)
       }
-    </ul>
+    </NavList>
     </div>
   )
 }
